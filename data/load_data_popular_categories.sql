ca1f7dbc44645b23af9d7554ea1208c4012a988f
unknown <admin@.(none)>
2009-12-18 11:43:41 +0530

    new files for displaying all categories

---
+++ data/load_data_popular_categories.sql
@@ -0,0 +1,10 @@
+use `ekkitab_books`;
+--
+-- Dumping data for table `ek_catalog_popular_categories`
+--
+
+INSERT INTO `ek_catalog_popular_categories` (`id`, `category`, `url_path`,  `order_no`, `is_active`,  `popularity`, `date`, `timestamp`) VALUES
+(1, 'Biographies & Memoirs', 'biographies-memoirs.html', 3, 1, 10,'2009-11-19 16:19:28', '2009-11-19 16:19:46'),
+(2, 'Literature & Fiction', 'literature-fiction.html', 1, 1, 15,'2009-11-19 16:20:31', '2009-11-19 16:20:56'),
+(3, 'Science Fiction & Fantasy','science-fiction-fantasy.html', 2, 1, 9,'2009-11-19 16:20:53', '2009-11-19 16:20:56'),
+(4, 'Autographs', 'Autographs.html', 4, 1, 13,'2009-11-19 14:18:57', '2009-11-19 14:18:57');
