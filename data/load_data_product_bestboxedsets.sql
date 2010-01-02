a67b2b4a68b232489bad8f3312323ec5ffff4735
unknown <admin@.(none)>
2010-01-02 23:23:17 +0530

    revised data

--- data/load_data_product_bestboxedsets.sql
+++ data/load_data_product_bestboxedsets.sql
@@ -6,7 +6,7 @@ use `ekkitab_books`;
 Delete from `ek_catalog_product_best_boxedsets`;
 
 INSERT INTO `ek_catalog_product_best_boxedsets` (`product_id`, `title`,  `author`, `image_url`,  `price`, `is_active`, `order_no`, `date`, `timestamp`) VALUES
-(36, "Irredeemable Ant-Man Volume 2 Digest","Charles","9/7/9780785119630.jpg",3.25, 1,1,'2009-11-19 16:20:53', '2009-11-19 16:20:56'),
-(37, "Essential Daredevil: Volume 4","David","9/7/9780785127628.jpg",6.25, 1, 2,'2009-11-19 14:18:57', '2009-11-19 14:18:57'),
-(38, "Essential X-Men Volume 8 Tpb", "Femer","9/7/9780785127635.jpg",8.25,1, 3,'2009-11-19 16:19:28', '2009-11-19 16:19:46'),
-(77, "We Are Paper Sisters Detective Company","Donald","9/7/9781421508627.jpg",1.25,1, 4,'2009-11-19 16:20:31', '2009-11-19 16:20:56');
+(56, "Arnobius Against the Heathen V6","Arnobius","9/7/9781419107634.jpg",9.25, 1,1,'2009-11-19 16:20:53', '2009-11-19 16:20:56'),
+(55, "Arnobius Against the Heathen V5","Arnobius","9/7/9781419107627.jpg",6.25, 1, 2,'2009-11-19 14:18:57', '2009-11-19 14:18:57'),
+(96, "A Monk's Alphabet: Moments of Stillness in a Turning World", "Jeremy Driscoll","9/7/9781590304624.jpg",8.25,1, 3,'2009-11-19 16:19:28', '2009-11-19 16:19:46'),
+(41, "Cuestionarios Increibles","Not Available","9/7/9780829751635.jpg",1.25,1, 4,'2009-11-19 16:20:31', '2009-11-19 16:20:56');
