1815727c9be8095d8820000078a99983c3d6f3f7
Vijay Raghavan <vijay@Zen.(none)>
2010-01-15 15:36:52 +0530

    this script runs the indexing.

---
+++ bin/create_index.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp ../java/bin:../java/lib/lucene-core-2.1.0.jar:../java/lib/lucene-memory-2.1.0.jar:../java/lib/mysql-connector-java-3.1.14-bin.jar BookIndex ../magento/search_index_dir
