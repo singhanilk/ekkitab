7acf0a9fcb801e3a2938da8d42c48275fad34b08
Vijay Raghavan <vijay@Zen.(none)>
2010-02-18 14:17:36 +0530

    Added script to create the books table in ekkitab_books and updated resetdb.sql to include this script.

--- db/resetdb.sql
+++ db/resetdb.sql
@@ -2,5 +2,6 @@ drop database if exists ekkitab_books;
 drop database if exists reference;
 source create_ekkitab_db.sql;
 source create_reference_db.sql;
+source create_books_in_ekkitab.sql;
 source ../data/ekkitab_categories.sql;
 source initdb.sql;
