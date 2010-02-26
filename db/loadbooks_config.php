461b8d0b294fd5f89ef0856a74d07f9e4f22a4a0
Vijay Raghavan <vijay@Zen.(none)>
2010-02-26 11:06:50 +0530

    dereferenced all path accesses through EKKITAB_HOME

--- db/loadbooks_config.php
+++ db/loadbooks_config.php
@@ -1,7 +1,8 @@
 <?php
-define("LOG4PHP_DIR", "/var/www/scm/extern/log4php");
-define("LOG4PHP_CONFIGURATION", "/var/www/scm/config/logger.properties");
-define("LOADBOOKS_INI", "/var/www/scm/config/loadbooks.ini");
+include("ekkitab_home.php");
+define("LOG4PHP_DIR", EKKITAB_HOME . "/extern/log4php");
+define("LOG4PHP_CONFIGURATION", EKKITAB_HOME . "/config/logger.properties");
+define("LOADBOOKS_INI", EKKITAB_HOME . "/config/loadbooks.ini");
 define("SELECT_LIMIT", 1000);
 define("FILE_ROLLOVER_LIMIT", 1000000);
 ?>
