1ff3ecfab7d898fbe2b47393740b7cc667627b7a
Vijay Raghavan <vijay@Zen.(none)>
2010-03-11 15:04:06 +0530

    importbooks changed to become the unified load script for all data - basic book information, descriptions, prices. the plugins have also been changed accordingly. added a new plugin to handle indian book data collected via google books.

--- db/importbooks_config.php
+++ db/importbooks_config.php
@@ -1,9 +1,14 @@
 <?php
 include("ekkitab_home.php");
-define("LOG4PHP_DIR", EKKITAB_HOME . "/extern/log4php");
-define("LOG4PHP_CONFIGURATION", EKKITAB_HOME . "/config/logger.properties");
-define("IMPORTBOOKS_INI", EKKITAB_HOME . "/config/importbooks.ini");
-define("IMPORTBOOKS_CLASSDIR", ".");
-define("IMPORTBOOKS_UNCLASSIFIED", "./unclassified_codes");
-define("BISAC_CODE_EQUIVALENTS", EKKITAB_HOME . "/db/" . "bisac_equivalents.txt");
+define (LOG4PHP_DIR, EKKITAB_HOME . "/extern/log4php");
+define (LOG4PHP_CONFIGURATION, EKKITAB_HOME . "/config/logger.properties");
+define (IMPORTBOOKS_INI, EKKITAB_HOME . "/config/importbooks.ini");
+define (IMPORTBOOKS_CLASSDIR, ".");
+define (IMPORTBOOKS_UNCLASSIFIED, "./unclassified_codes");
+define (BISAC_CODE_EQUIVALENTS, EKKITAB_HOME . "/db/" . "bisac_equivalents.txt");
+define (IMPORTBOOKS_PLUGINS_DIR, EKKITAB_HOME . "/db/" . "plugins");
+define (MODE_BASIC, 0x01);
+define (MODE_PRICE, 0x02);
+define (MODE_DESC, 0x04);
+define (MAX_DESCRIPTION_LENGTH, 1024);
 ?>
--- db/init_refdb.sql
+++ db/init_refdb.sql
@@ -1,17 +1,37 @@
 use reference;
 
 --
--- Dumping data for table `ek_currency_conversion`
+-- Seed data for table `ek_currency_conversion`
 --
 
-insert into ek_currency_conversion (`currency`, `conversion`) values ('USD', 50.0);
-insert into ek_currency_conversion (`currency`, `conversion`) values ('INR', 1.0);
-
--- -------------------------------------------------------------------------------------
+insert into ek_currency_conversion (`currency`, `conversion`) values ('USD', 50.0), ('INR', 1.0), ('BPS', 80.0);
 
 --
--- Dumping data for table `ek_discount_setting`
+-- Seed data for table `ek_discount_setting`
 --
 
-insert into ek_discount_setting (`info_source`, `discount_percent`) values ('Ingrams', 20);
-insert into ek_discount_setting (`info_source`, `discount_percent`) values ('1ktestdata', 30);
+insert into ek_discount_setting (`info_source`, `discount_percent`) values ('Ingrams', 20), ('1ktestdata', 30), ('Penguin', 25);
+
+--
+-- Seed data for table `ek_supplier_params`
+--
+
+insert into ek_supplier_params (`info_source`, `publisher`, `supplier_discount`, `delivery_period`) values 
+                               ('Penguin', 'Dorling Kindersley', 21, 1),
+                               ('Penguin', 'Penguin', 22, 2),
+                               ('Penguin', 'Alistair Sawday', 23, 3),
+                               ('Penguin', 'Allen Lane', 24, 4),
+                               ('Penguin', 'Arkana', 35, 15),
+                               ('Penguin', 'Claremont', 35, 15),
+                               ('Penguin', 'Alpha', 35, 15),
+                               ('Penguin', 'Daedalus Books', 35, 15),
+                               ('Penguin', 'Fig Tree', 35, 15),
+                               ('Penguin', 'Hamish Hamilton', 35, 15),
+                               ('Penguin', 'HardWired', 35, 15),
+                               ('Penguin', 'Helicon', 35, 15),
+                               ('Penguin', 'LadyBird', 35, 15),
+                               ('Penguin', 'Michael Joseph', 35, 15),
+                               ('Penguin', 'Pelham', 35, 15),
+                               ('Penguin', 'Peng Mod Classics', 35, 15),
+                               ('Penguin', 'Peng Tcc Classics', 35, 15),
+                               ('Penguin', 'BBC', 35, 15);
