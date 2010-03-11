fb38223b53b046c6c12436c10402de79723012ce
Vijay Raghavan <vijay@Zen.(none)>
2010-03-11 16:09:52 +0530

    Corrected reference to importbooks_config file.

--- db/plugins/Ingrams.php
+++ db/plugins/Ingrams.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL  & ~E_NOTICE);
 ini_set("display_errors", 1); 
-include("importbooks_config.php");
+include(EKKITAB_HOME . "/" . "db" . "/" . "importbooks_config.php");
 
 //  
 //
