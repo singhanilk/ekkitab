deb72566f62e6717a627931b9222a99d2d108d4b
Anil Singh <anil@ekkitab.com>
2010-03-19 17:00:28 +0530

    Added cancel in local ekkitab and changed the logic to Not on mc_gross

--- magento/app/code/local/Ekkitab/Paypal/Model/Standard.php
+++ magento/app/code/local/Ekkitab/Paypal/Model/Standard.php
@@ -289,6 +289,162 @@ public function getStandardCheckoutFormFields()
         }
         return $rArr;
     }
+    
+     public function ipnPostSubmit()
+    {
+        $sReq = '';
+        $sReqDebug = '';
+        foreach($this->getIpnFormData() as $k=>$v) {
+            $sReq .= '&'.$k.'='.urlencode(stripslashes($v));
+            $sReqDebug .= '&'.$k.'=';
+
+        }
+        //append ipn commdn
+        $sReq .= "&cmd=_notify-validate";
+        $sReq = substr($sReq, 1);
+
+        if ($this->getDebug()) {
+            $debug = Mage::getModel('paypal/api_debug')
+                    ->setApiEndpoint($this->getPaypalUrl())
+                    ->setRequestBody($sReq)
+                    ->save();
+        }
+        $http = new Varien_Http_Adapter_Curl();
+        $http->write(Zend_Http_Client::POST,$this->getPaypalUrl(), '1.1', array(), $sReq);
+        $response = $http->read();
+        $response = preg_split('/^\r?$/m', $response, 2);
+        $response = trim($response[1]);
+        if ($this->getDebug()) {
+            $debug->setResponseBody($response)->save();
+        }
+
+         //when verified need to convert order into invoice
+        $id = $this->getIpnFormData('invoice');
+        $order = Mage::getModel('sales/order');
+        $order->loadByIncrementId($id);
+
+        if ($response=='VERIFIED') {
+            if (!$order->getId()) {
+                /*
+                * need to have logic when there is no order with the order id from paypal
+                */
+
+            } else {
+                          //I HAVE CHANGED THIS to ! as the return amount is in dollar whereas the amount stored is in RS
+                if (!($this->getIpnFormData('mc_gross')!=$order->getBaseGrandTotal())) {
+                    //when grand total does not equal, need to have some logic to take care
+                    $order->addStatusToHistory(
+                        $order->getStatus(),//continue setting current order status
+                        Mage::helper('paypal')->__('Order total amount does not match paypal gross total amount')
+                    );
+                    $order->save();
+                } else {
+                    /*
+                    //quote id
+                    $quote_id = $order->getQuoteId();
+                    //the customer close the browser or going back after submitting payment
+                    //so the quote is still in session and need to clear the session
+                    //and send email
+                    if ($this->getQuote() && $this->getQuote()->getId()==$quote_id) {
+                        $this->getCheckout()->clear();
+                        $order->sendNewOrderEmail();
+                    }
+                    */
+
+                    // get from config order status to be set
+                    $newOrderStatus = $this->getConfigData('order_status', $order->getStoreId());
+                    if (empty($newOrderStatus)) {
+                        $newOrderStatus = $order->getStatus();
+                    }
+
+                    /*
+                    if payer_status=verified ==> transaction in sale mode
+                    if transactin in sale mode, we need to create an invoice
+                    otherwise transaction in authorization mode
+                    */
+                    if ($this->getIpnFormData('payment_status') == 'Completed') {
+                       if (!$order->canInvoice()) {
+                           //when order cannot create invoice, need to have some logic to take care
+                           $order->addStatusToHistory(
+                                $order->getStatus(), // keep order status/state
+                                Mage::helper('paypal')->__('Error in creating an invoice', true),
+                                $notified = true
+                           );
+
+                       } else {
+                           //need to save transaction id
+                           $order->getPayment()->setTransactionId($this->getIpnFormData('txn_id'));
+                           //need to convert from order into invoice
+                           $invoice = $order->prepareInvoice();
+                           $invoice->register()->pay();
+                           Mage::getModel('core/resource_transaction')
+                               ->addObject($invoice)
+                               ->addObject($invoice->getOrder())
+                               ->save();
+                           $order->setState(
+                               Mage_Sales_Model_Order::STATE_COMPLETE, true,
+                               Mage::helper('paypal')->__('Invoice #%s created', $invoice->getIncrementId()),
+                               $notified = true
+                           );
+                       }
+                    } else {
+                        $order->setState(
+                            Mage_Sales_Model_Order::STATE_PROCESSING, $newOrderStatus,
+                            Mage::helper('paypal')->__('Received IPN verification'),
+                            $notified = true
+                        );
+                    }
+
+                    $ipnCustomerNotified = true;
+                    if (!$order->getPaypalIpnCustomerNotified()) {
+                        $ipnCustomerNotified = false;
+                        $order->setPaypalIpnCustomerNotified(1);
+                    }
+
+                    $order->save();
+
+                    if (!$ipnCustomerNotified) {
+                        $order->sendNewOrderEmail();
+                    }
+
+                }//else amount the same and there is order obj
+                //there are status added to order
+            }
+        }else{
+            /*
+            Canceled_Reversal
+            Completed
+            Denied
+            Expired
+            Failed
+            Pending
+            Processed
+            Refunded
+            Reversed
+            Voided
+            */
+            $payment_status= $this->getIpnFormData('payment_status');
+            $comment = $payment_status;
+            if ($payment_status == 'Pending') {
+                $comment .= ' - ' . $this->getIpnFormData('pending_reason');
+            } elseif ( ($payment_status == 'Reversed') || ($payment_status == 'Refunded') ) {
+                $comment .= ' - ' . $this->getIpnFormData('reason_code');
+            }
+            //response error
+            if (!$order->getId()) {
+                /*
+                * need to have logic when there is no order with the order id from paypal
+                */
+            } else {
+                $order->addStatusToHistory(
+                    $order->getStatus(),//continue setting current order status
+                    Mage::helper('paypal')->__('Paypal IPN Invalid %s.', $comment)
+                );
+                $order->save();
+            }
+        }
+    }
+    
 	
 	/*public function getStandardCheckoutFormFields()
     {
--- magento/app/code/local/Ekkitab/Paypal/controllers/StandardController.php
+++ magento/app/code/local/Ekkitab/Paypal/controllers/StandardController.php
@@ -86,6 +86,67 @@ class Ekkitab_Paypal_StandardController extends Mage_Paypal_StandardController
        		$this->_redirect('checkout/multishipping/success');
         
     }
+    
+    
+    
+    /**
+     * When a customer cancel payment from paypal.
+     */
+    public function cancelAction()
+    {
+        $session = Mage::getSingleton('checkout/session');
+        $session->setQuoteId($session->getPaypalStandardQuoteId(true));
+
+        // cancel order
+     /*   if ($session->getLastRealOrderId()) {
+            $order = Mage::getModel('sales/order')->loadByIncrementId($session->getLastRealOrderId());
+            if ($order->getId()) {
+                $order->cancel()->save();
+            }
+        }
+        */
+ 
+          if (Mage::getSingleton('checkout/session')->getQuote()->getIsMultiShipping()){
+                
+                    $Merchant_Param="M" ; 
+                    Mage::log("/n".__FILE__."(".__LINE__.")".__METHOD__."AKS _Checkout_Model_Type_Multishipping\n") ;
+                      
+    				$Order_Ids    =  Mage::getSingleton('core/session')->getOrderIds();
+	                Mage::log("/n".__FILE__."(".__LINE__.")".__METHOD__."Multiship Returned from Paypal\n".print_r($Order_Ids,true)) ;
+	                
+          }
+          else {
+                      $Merchant_Param="S" ; 
+                      Mage::log("/n".__FILE__."(".__LINE__.")".__METHOD__."Akse_Checkout_Model_Type_Onepage\n") ;
+                      
+                      $Order_Id=  Mage::getSingleton('checkout/session')->getQuote()->getLastRealOrderId();  // for single shipment order 
+                      Mage::log("/n".__FILE__."(".__LINE__.")".__METHOD__."Singleship Returned from Paypal\n".print_r($Order_Id,true)) ;
+                      $Order_Ids[] = $Order_Id ;
+                      
+                      
+       	 }
+       	  foreach($Order_Ids as $key => $orid ) {
+          	                Mage::log("/n".__FILE__."(".__LINE__.")".__METHOD__."\n".print_r($orid,true)) ;
+         	 $Order_Id = $orid ;
+      	 	 $order = Mage::getModel('sales/order');
+      	 	 $order->loadByIncrementId($Order_Id);  // 
+       	 	 if ($order->getId()) {
+                $order->cancel()->save();
+            }
+      	 	 
+       	  }
+    
+        /*we are calling getPaypalStandardQuoteId with true parameter, the session object will reset the session if parameter is true.
+        so we don't need to manually unset the session*/
+        //$session->unsPaypalStandardQuoteId();
+
+        //need to save quote as active again if the user click on cacanl payment from paypal
+        //Mage::getSingleton('checkout/session')->getQuote()->setIsActive(true)->save();
+        //and then redirect to checkout one page
+        $this->_redirect('checkout/cart');
+    }
+    
 
       
 }
+
