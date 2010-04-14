02b005c66b9c3071fbd469d8f06e7d499565079c
Anil Singh <anil@ekkitab.com>
2010-04-14 16:51:37 +0530

    testing

--- db/backup.bat
+++ db/backup.bat
@@ -1,16 +1,16 @@
 set dbhost=%1
 set uid=%2
 set pswd=%3
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity > %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_datetime >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_decimal >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_int >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_text >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_varchar >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_datetime >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_decimal >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_int >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_text >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_varchar >> %EKKITAB_HOME%/data/backup.sql ;
-mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books eav_entity_store >> %EKKITAB_HOME%/data/backup.sql ;
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity > %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_datetime >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_decimal >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_int >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_text >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_entity_varchar >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_datetime >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_decimal >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_int >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_text >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books customer_address_entity_varchar >> %EKKITAB_HOME%/data/backup.sql 
+mysqldump -h %dbhost% -u %uid% -p%pswd% ekkitab_books eav_entity_store >> %EKKITAB_HOME%/data/backup.sql 
--- db/reset_ekkitab_books.bat
+++ db/reset_ekkitab_books.bat
@@ -7,5 +7,5 @@ echo calling %EKKITAB_HOME%/db/backup.bat %dbhost% %uid% %pswd%
 call %EKKITAB_HOME%/db/backup.bat %dbhost% %uid% %pswd%
 echo Resetting database and restoring backed up data...
 mysql -s -h %dbhost% -u %uid% -p%pswd% < %EKKITAB_HOME%/db/reset_ekkitab_books.sql
-mysql -s -h %dbhost% -u %uid% -p%pswd% < %EKKITAB_HOME%/data/backup.sql 
+mysql -s -h %dbhost% -u %uid% -p%pswd% ekkitab_books < %EKKITAB_HOME%/data/backup.sql 
 
