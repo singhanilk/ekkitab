22a44464a2acbd408eea16d16f5f35736df8253a
Vijay Raghavan <vijay@Zen.(none)>
2010-04-19 09:12:58 +0530

    Changed to sit modifications to package structure.

--- bin/create_index.sh
+++ bin/create_index.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . $EKKITAB_HOME/bin/db.sh 
-java -Xmx256m -cp ../java/bin:../java/lib/lucene-core-2.4.0.jar:../java/lib/lucene-memory-2.4.0.jar:../java/lib/mysql-connector-java-3.1.14-bin.jar BookIndex ../magento/search_index_dir ../magento/categories.xml "true" $host $user $password 0 
+java -Xmx256m -cp ../java/bin:../java/lib/lucene-core-2.4.0.jar:../java/lib/lucene-memory-2.4.0.jar:../java/lib/mysql-connector-java-3.1.14-bin.jar:../java/lib/lucene-spellchecker-2.4.0.jar:../java/bin/ekkitabsearch.jar com.ekkitab.search.BookIndex ../magento/search_index_dir ../magento/categories.xml "true" $host $user $password 0 
