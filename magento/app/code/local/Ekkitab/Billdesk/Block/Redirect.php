5ff620f85d55528a986ab6723256bc11d8535789
Anil Singh <anil@ekkitab.com>
2010-04-27 14:06:03 +0530

    made page blank instead of "redirected to ....."

--- magento/app/code/local/Ekkitab/Billdesk/Block/Redirect.php
+++ magento/app/code/local/Ekkitab/Billdesk/Block/Redirect.php
@@ -26,7 +26,9 @@ class Ekkitab_Billdesk_Block_Redirect extends Mage_Core_Block_Abstract
         
         
         $html = '<html><body>';
-        $html.= $this->__('You will be redirected to Billdesk Payment Gateway in a few seconds.');
+    //    $html.= $this->__('You will be redirected to Billdesk Payment Gateway in a few seconds.');
+         $html.= $this->__('');
+        
         $html.= $form->toHtml();
         $html.= '<script type="text/javascript">document.getElementById("billdesk_checkout").submit();</script>';
         $html.= '</body></html>';
