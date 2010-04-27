da7cef70824acc02baee6de3d434ad058cfc4f86
root <root@anils-laptop.(none)>
2010-04-27 11:38:04 +0530

    added uload.sh to load all software on ubuntu

---
+++ bin/uload.sh
@@ -0,0 +1,17 @@
+apt-get update
+apt-get install git-core
+apt-get install php5-cli
+apt-get install php5-curl
+apt-get install apache2
+apt-get install tomcat6
+apt-get install mysql-server
+apt-get install php5-mysql
+apt-get install phpmyadmin
+apt-get install ec2-api-tools
+apt-get install ruby
+apt-get install php-apc
+apt-get install memcached
+apt-get install php5-memcache
+apt-get install php-pear
+pear install -f System_Daemon
+
