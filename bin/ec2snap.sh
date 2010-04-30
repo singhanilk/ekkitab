0c60af6ef3124eabb10e9cc5223d489c7dee465b
Anil Singh <anil@ekkitab.com>
2010-04-30 11:53:59 +0530

    added Net::Amazon::EC2

--- bin/ec2snap.sh
+++ bin/ec2snap.sh
@@ -4,4 +4,6 @@ echo "deb http://ppa.launchpad.net/alestic/ppa/ubuntu $codename main"| sudo tee 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BE09C571
 sudo apt-get update
 sudo apt-get install -y ec2-consistent-snapshot
+sudo PERL_MM_USE_DEFAULT=1 cpan Net::Amazon::EC2
+
 
