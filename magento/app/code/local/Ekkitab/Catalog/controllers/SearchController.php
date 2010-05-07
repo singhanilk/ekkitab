bd4cc8c39ff8be441e2566dc578ca9bbee951824
unknown <admin@.(none)>
2010-05-07 15:56:37 +0530

    some debug statements

--- magento/app/code/local/Ekkitab/Catalog/controllers/SearchController.php
+++ magento/app/code/local/Ekkitab/Catalog/controllers/SearchController.php
@@ -21,10 +21,8 @@ class Ekkitab_Catalog_SearchController extends Mage_Core_Controller_Front_Action
 		$categoryPath = Mage::helper('ekkitab_catalog')->getCurrentCategoryPath();
 		// @var $query Mage_CatalogSearch_Model_Query
 		$queryText = Mage::helper('ekkitab_catalog')->getQueryText();
-		
-		
+		$start=(float)microtime(true);
 		if ((isset($categoryPath) && strlen($categoryPath) > 0 ) || strlen($queryText) > 0 ) {
-			Mage::log(" In SearchController : Start : for search string : '".$queryText."' and category: '".$categoryPath."' @ time:".date('H:m:s'));
 			if (strlen($categoryPath) > 0 ) {
                 Mage::getSingleton('core/session')->setCurrentCategoryPath(array('current_category_path'=>$categoryPath));
 			}else{
@@ -70,7 +68,10 @@ class Ekkitab_Catalog_SearchController extends Mage_Core_Controller_Front_Action
 			if (!Mage::helper('ekkitab_catalog')->isMinQueryLength()) {
 				$query->save();
 			}
-			Mage::log(" In SearchController : End : for search string : '".$queryText."' and category: '".$categoryPath."' @ time:".date('H:m:s'));
+			$end=(float)microtime(true);
+			$blockTimerArr= Mage::getSingleton('core/session')->getBlockDebugTimer();
+			$phtmlTimerArr= Mage::getSingleton('core/session')->getPhtmlDebugTimer();
+			Mage::log("Debug Times From controller to start of block: ".sprintf("%.5f", (float)($blockTimerArr['block1']-$start))." : to initialization of java class : ".sprintf("%.5f", (float)($blockTimerArr['block2']-$start))." : to searching of book :  ".sprintf("%.5f", (float)($blockTimerArr['block3']-$start))." : to fetch books :".sprintf("%.5f", (float)($blockTimerArr['block4']-$start))." : to start phtml rendering : ".sprintf("%.5f", (float)($phtmlTimerArr['start']-$start))." : to finish rendering : ".sprintf("%.5f", (float)($phtmlTimerArr['end']-$start))." : total time :".sprintf("%.5f", (float)($end-$start)));
 		}
         else {
             $this->_redirectReferer();
