ea3261bb0ba17d9b52e53dd0240fcd5c34fc5874
Vijay Raghavan <vijay@Zen.(none)>
2010-05-11 10:49:25 +0530

    Revert "new perl script to run a test "runtest.pl""
    This reverts commit 58f9271bbaafd2b715034261dd6442eeb295795c.

--- bin/jmeter.sh
+++ bin/jmeter.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # invoke the jmeter testscript 10searches.jmx , that bombards ekkitab.co.in with 100 virtual users in under a second
 if [ $# -ne 1 ]; then
-echo "Not Enough arguments! Enter the path where the jmeter test script is stored"; echo "Usage : $0 <jmeterTestScript> $1 <number_of_threads> $2 <ramp_up_time> $3"
+echo "Not Enough arguments! Enter the path where the jmeter test script is stored"; echo "Usage : $0 <jmeterTestScript>"
 exit 1;
 fi;
-
 echo " Launching jmeter"
 jmeter -n -t $1
+
+
--- bin/runtest.pl
+++ bin/runtest.pl
@@ -1,37 +1,15 @@
 #!/usr/bin/perl
 sub main {
-
-   if($ENV{'EKKITAB_HOME'} eq "")
-   {
-	die "EKKITAB_HOME not set!"
-   }
-   my $threads  = 10;
-   my $ramptime = 5;
-   my $logfile  = "$ENV{'EKKITAB_HOME'}/logs/jmeter_test_output.jtl";
-   
-
-   for($i=0;$i<@ARGV;$i+=2) {
-       my($arg) = $ARGV[$i];
-       if ($arg eq "-t") {
-         $threads = $ARGV[$i+1];
-       }
-       elsif ($arg eq '-r') {
-         $ramptime = $ARGV[$i+1];
-       }
-       elsif ($arg eq '-o') {
-         $logfile = "$ENV{'EKKITAB_HOME'}/logs/" . $ARGV[$i+1];
-       }
-       else {
-	 die "Unknown argument $arg\n";
-       }
-   }
-
-   print "Executing jmeter with the following parameters \n Number of threads : $threads \n Ramp up Perid : $ramptime \n Output Log File : $logfile\n";
-
-
-    
+    if ($#ARGV != 1) {
+        print "Insufficient arguments!\n";
+        print "Usage: runtest.pl <num threads> <rampup time>\n";
+        exit(1);
+    }
+    my $threads  = $ARGV[0];
+    my $ramptime = $ARGV[1];
+  
     my $file = "$ENV{'EKKITAB_HOME'}/test/jmeter-test-template";
-    my $outputfile = "$ENV{'EKKITAB_HOME'}/test/jmeter_test_" . $threads . "_" . $ramptime . ".jmx";
+    my $outputfile = "$ENV{'EKKITAB_HOME'}" . "/test/jmeter_test_" . $threads . "_" . $ramptime . ".jmx";
 
     open (FILE, $file) or die "Cannot open test template";
     open (OUTPUT, ">", $outputfile) or die "Cannot open $outputfile to write test script.";
@@ -41,15 +19,10 @@ sub main {
 
     $contents =~ s/NUMBEROFTHREADS/$threads/;
     $contents =~ s/RAMPUPTIME/$ramptime/;
-    $contents =~ s/OUTPUTFILE/$logfile/;
 
     print OUTPUT $contents;
 
     close(OUTPUT);
     close(FILE);
-    my $cmd = "$ENV{'EKKITAB_HOME'}/extern/jmeter/bin/jmeter";
-    my $jmeterlog = "$ENV{'EKKITAB_HOME'}/logs/jmeter.log";
-
-    my $jmeterTestRun = system("$cmd -n -t $outputfile"); #-l $jmeterlog");
 }
 main();
