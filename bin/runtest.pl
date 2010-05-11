e4082523c810cb54947385ee9be564c6f949c2f4
Vijay Raghavan <vijay@Zen.(none)>
2010-05-11 11:30:19 +0530

    added target domain to test script.

--- bin/runtest.pl
+++ bin/runtest.pl
@@ -8,6 +8,7 @@ sub main {
    my $threads  = 10;
    my $ramptime = 5;
    my $logfile  = "$ENV{'EKKITAB_HOME'}/logs/jmeter_test_output.jtl";
+   my $target   = "www.ekkitab.co.in";
    
 
    for($i=0;$i<@ARGV;$i+=2) {
@@ -21,12 +22,18 @@ sub main {
        elsif ($arg eq '-o') {
          $logfile = "$ENV{'EKKITAB_HOME'}/logs/" . $ARGV[$i+1];
        }
+       elsif ($arg eq '-d') {
+         $target = $ARGV[$i+1];
+       }
        else {
 	 die "Unknown argument $arg\n";
        }
    }
 
-   print "Executing jmeter with the following parameters \n Number of threads : $threads \n Ramp up Perid : $ramptime \n Output Log File : $logfile\n";
+   print "Executing jmeter with the following parameters \n Number of threads : $threads \n" .
+         "Ramp up Perid : $ramptime \n" .
+         "Output Log File : $logfile\n" .
+         "Target system : $target\n"; 
 
 
     
@@ -42,6 +49,7 @@ sub main {
     $contents =~ s/NUMBEROFTHREADS/$threads/;
     $contents =~ s/RAMPUPTIME/$ramptime/;
     $contents =~ s/OUTPUTFILE/$logfile/;
+    $contents =~ s/TARGETSYSTEM/$target/;
 
     print OUTPUT $contents;
 
