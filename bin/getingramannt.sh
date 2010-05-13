95522a0eb78bcf905ad855ccd4cc4a7067abf14d
Vijay Raghavan <vijay@Zen.(none)>
2010-05-13 07:35:53 +0530

    removed unzip. the file is not zipped!

--- bin/getingramannt.sh
+++ bin/getingramannt.sh
@@ -6,8 +6,6 @@ targetfile="antingrm-13.txt"
 echo "getting file $targetfile ...."
 (cd $targetdir; mkdir $datestr)
 (cd $targetdir/$datestr; wget -O $targetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/titleswk/$targetfile)
-echo "unzipping file $targetfile ..."
-(cd $targetdir/$datestr; unzip $targetfile)
 
 
 
