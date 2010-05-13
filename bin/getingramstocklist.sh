30f7c4654bf33961392472c1b85df095316a6353
Vijay Raghavan <vijay@Zen.(none)>
2010-05-13 07:43:21 +0530

    remove existing data files before unzip

--- bin/getingramstocklist.sh
+++ bin/getingramstocklist.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 targetdir="/mnt4/publisherdata/Ingram"
 targetfile="stockv2@ingram.zip"
+datafile="stockv2@ingram.dat"
 echo "getting file $targetfile ...."
 (cd $targetdir; wget -O $targetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/Inventory/$targetfile)
 echo "unzipping file $targetfile ..."
-(cd $targetdir; unzip $targetfile)
+(cd $targetdir; rm -f $datafile; unzip $targetfile)
 
 
 
--- bin/getingramtitles.sh
+++ bin/getingramtitles.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 targetdir="/mnt4/publisherdata/Ingram"
 targetfile="ttlingv2.zip"
+datafile="ttlingv2.txt"
 echo "getting file $targetfile ...."
 (cd $targetdir; wget -O $targetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/titleswk/$targetfile)
 echo "unzipping file $targetfile ..."
-(cd $targetdir; unzip $targetfile)
+(cd $targetdir; rm -f $datafile; unzip $targetfile)
 
 
 
