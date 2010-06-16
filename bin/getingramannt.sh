5cf0edd4cb31070b71c14c2ac5062f5bac30bebf
Vijay Raghavan <vijay@Zen.(none)>
2010-06-16 22:20:36 +0530

    Fixes date extraction from Ingrams stock file. Ingram book annotations are now pulled into the same directory and catalog.cfg is updated.

--- bin/getingramannt.sh
+++ bin/getingramannt.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 datestr=`date +%B%Y`
 targetdir="/mnt4/publisherdata/Ingram"
-targetfile="antingrm-13.txt"
+localtargetfile="antingrm-13-$datestr.txt"
+ftptargetfile="antingrm-13.txt"
+catalogfile="/mnt2/scm/config/catalog.cfg"
 
 echo "getting file $targetfile ...."
-(cd $targetdir; mkdir $datestr)
-(cd $targetdir/$datestr; wget -O $targetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/titleswk/$targetfile)
+#(cd $targetdir; mkdir $datestr)
+#(cd $targetdir/$datestr; wget -O $targetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/titleswk/$targetfile)
+(cd $targetdir; wget -O $localtargetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/titleswk/$ftptargetfile)
+echo "-d Ingrams $targetdir/$localtargetfile" >> $catalogfile
 
 
 
