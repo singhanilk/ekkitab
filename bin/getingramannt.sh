92232fcbd1edcf8614479d89bae44ff34709d6ea
Vijay Raghavan <vijay@Zen.(none)>
2010-06-17 12:44:58 +0530

    Updates the catalog config file only if needed.

--- bin/getingramannt.sh
+++ bin/getingramannt.sh
@@ -5,11 +5,14 @@ localtargetfile="antingrm-13-$datestr.txt"
 ftptargetfile="antingrm-13.txt"
 catalogfile="/mnt2/scm/config/catalog.cfg"
 
-echo "getting file $targetfile ...."
+echo "getting file $ftptargetfile ...."
 #(cd $targetdir; mkdir $datestr)
 #(cd $targetdir/$datestr; wget -O $targetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/titleswk/$targetfile)
 (cd $targetdir; wget -O $localtargetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/titleswk/$ftptargetfile)
-echo "-d Ingrams $targetdir/$localtargetfile" >> $catalogfile
+if ( ! grep "$targetdir/$localtargetfile" $catalogfile >/dev/null 2>&1 ) ; then
+   echo "-d Ingrams $targetdir/$localtargetfile" >> $catalogfile
+fi
+
 
 
 
