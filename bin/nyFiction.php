fceb3ec0b67e3b87540c45f1489b03f6cb06d88a
Nimil Christopher <chris@Arun-laptop.(none)>
2010-06-18 17:56:57 +0530

    modified the nyAPI parser to suit menu.xml

--- bin/nyFiction.php
+++ bin/nyFiction.php
@@ -22,7 +22,7 @@ function startElement($parser, $name, $attrs) {
 function endElement($parser, $name) {
 	global $insideitem, $tag, $title, $description, $isbn13, $isbn10, $author, $weeksOnList, $thisWeek;
 		if ($name == "BOOK") {
-		echo $isbn13 . "\t" . $weeksOnList . "\t" . $thisWeek . "\n";
+		echo "\t<BOOK id=" . $isbn13. " title=". $title. " author=". $author. "/>\n";
 		$title = "";
 		$description = "";
 		$isbn10 = "";
@@ -69,12 +69,13 @@ xml_set_character_data_handler($xml_parser, "characterData");
 $fp = fopen("http://api.nytimes.com/svc/books/v2/lists.xml?list=trade-fiction-paperback&api-key=7445911964368f98ebf72c87c2d12913:19:60451432","rb")
 	or die("Error reading RSS data.");
 print "ISBN\tWeeks On List\tCurrent Rank\n"; 
+echo "<SECTION title=\"New York Times BestSellers (Fiction)\" type=\"collection\" showall=\"yes\" highlight=\"5\" shuffle=\"yes\">\n";
 while ($data = fread($fp, 3355443))
 	xml_parse($xml_parser, $data, feof($fp))
 		or die(sprintf("XML error: %s at line %d", 
 			xml_error_string(xml_get_error_code($xml_parser)), 
 			xml_get_current_line_number($xml_parser)));
-                        
+echo "</SECTION>\n";                       
 fclose($fp);
 xml_parser_free($xml_parser);
 
