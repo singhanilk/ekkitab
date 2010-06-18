8a343b61b9c18d0039190185756f0971a0c465bd
unknown <admin@.(none)>
2010-06-18 17:06:25 +0530

    fixed a bug

--- bin/create_categories_array.sh
+++ bin/create_categories_array.sh
@@ -4,6 +4,6 @@ if [ -z $EKKITAB_HOME ] ; then
     exit 1;
 fi;
 echo "generating categories array ...."
-(cd $EKKITAB_HOME/bin; php generatecategories_array.php -i ../categories.xml -o ../tmp)
+(cd $EKKITAB_HOME/bin; php generatecategories_array.php -i ../magento/categories.xml -o ../tmp)
 echo "copying categories to production location ...."
 (cd $EKKITAB_HOME/tmp; cp categories.php ../magento/app/design/frontend/default/ekkitab/template/catalog/category/categories.php)
