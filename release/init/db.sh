2934f09a4593cf52889fb6c82dcc2dc179508e27
Vijay Raghavan <vijay@Zen.(none)>
2010-06-18 22:30:54 +0530

    Removed duplication of scripts. A new script will be written to create the init release package by copying the right scripts from bin to the init directory.

--- release/init/db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dbconfig=`$EKKITAB_HOME/bin/readdbconfig.pl`
-host=`echo $dbconfig | cut -d' ' -f1 | while read a; do echo $a; done`;
-user=`echo $dbconfig | cut -d' ' -f2 | while read a; do echo $a; done`;
-password=`echo $dbconfig | cut -d' ' -f3 | while read a; do echo $a; done`;
-
