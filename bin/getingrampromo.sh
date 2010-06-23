afaf73aef782c50cc9ea1b11fe6094248dd594d9
Vijay Raghavan <vijay@Zen.(none)>
2010-06-23 11:15:07 +0530

    Removed debug.

--- bin/getingrampromo.sh
+++ bin/getingrampromo.sh
@@ -7,9 +7,6 @@ filename="pmmnthy-13.txt"
 savedfilename=${filename%.*}-$datestr.txt
 catalogfile="/mnt2/scm/config/catalog.cfg"
 
-echo $savedfilename
-exit 1
-
 echo "getting file $ftptargetfile ...."
 (cd $targetdir; wget -O $localtargetfile ftp://w20M0695:ees695@ftp1.ingrambook.com/titleswk/$ftptargetfile)
 echo "unzipping received zip file..."
