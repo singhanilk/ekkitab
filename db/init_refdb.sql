d38d8e35a4c8f4118ffb639b3a36de80d71f1496
Vijay Raghavan <vijay@Zen.(none)>
2010-06-24 11:03:15 +0530

    bugfixes to IndiaPublisher plugin. updates to menu and global sections.

--- db/init_refdb.sql
+++ db/init_refdb.sql
@@ -4,45 +4,51 @@ use reference;
 -- Seed data for table `ek_currency_conversion`
 --
 
-insert into ek_currency_conversion (`currency`, `conversion`) values ('USD', 50.0), ('INR', 1.0), ('BRI', 80.0), ('CAN', 40);
+insert into ek_currency_conversion (`currency`, `conversion`) values ('USD', 48.20), ('INR', 1.0), ('BRI', 71.20), ('CAN', 40);
 
 --
 -- Seed data for table `ek_discount_setting`
 --
 
-insert into ek_discount_setting (`info_source`, `discount_percent`) values ('Ingrams', 20), ('1ktestdata', 30), 
-                                                                           ('Penguin', 25), ('Unknown', 25),
-                                                                           ('Amit', 25), ('Dolphin', 25),
-                                                                           ('HarperCollins', 25), ('Hachette', 25),
-                                                                           ('Jaico', 25), ('MediaStar', 25),
-                                                                           ('NewIndiaBookSource', 25), ('OrientBlackSwan', 25),
-                                                                           ('Oxford', 25), ('PearsonEducation', 25),
-                                                                           ('PopularPrakasham', 25), ('Prakash', 25),
-                                                                           ('Prism', 25), ('RandomHouse', 25),
-                                                                           ('ResearchPress', 25), ('Rupa', 25),
-                                                                           ('Sage', 25);
+insert into ek_discount_setting (`info_source`, `discount_percent`) values ('Ingrams', 50), ('1ktestdata', 30), 
+                                                                           ('Penguin', 50), ('Unknown', 50),
+                                                                           ('Amit', 70), ('Dolphin', 70),
+                                                                           ('HarperCollins', 70), ('Hachette', 70),
+                                                                           ('Jaico', 70), ('MediaStar', 70),
+                                                                           ('NewIndiaBookSource', 70), ('OrientBlackSwan', 70),
+                                                                           ('Oxford', 70), ('PearsonEducation', 70),
+                                                                           ('PopularPrakasham', 70), ('Prakash', 70),
+                                                                           ('Prism', 70), ('RandomHouse', 70),
+                                                                           ('ResearchPress', 70), ('Rupa', 70),
+                                                                           ('Vinayaka', 70), ('Westland', 70),
+                                                                           ('CambridgeUniversityPress', 70), ('IndiaBooks', 70),
+                                                                           ('Sage', 70);
 
 --
 -- Seed data for table `ek_supplier_params`
 --
 
 insert into ek_supplier_params (`info_source`, `publisher`, `supplier_discount`, `delivery_period`) values 
-                               ('Penguin', 'Any', 20, 5),
-                               ('Jaico', 'Any', 20, 5),
-                               ('Amit', 'Any', 20, 5),
-                               ('Dolphin', 'Any', 20, 5),
-                               ('HarperCollins', 'Any', 20, 5),
-                               ('Hachette', 'Any', 20, 5),
-                               ('MediaStar', 'Any', 20, 5),
-                               ('NewIndiaBookSource', 'Any', 20, 5),
-                               ('OrientBlackSwan', 'Any', 20, 5),
-                               ('Oxford', 'Any', 20, 5),
-                               ('PearsonEducation', 'Any', 20, 5),
+                               ('Penguin', 'Any', 33, 5),
+                               ('Jaico', 'Any', 33, 5),
+                               ('Amit', 'Any', 30, 5),
+                               ('Dolphin', 'Any', 35, 5),
+                               ('HarperCollins', 'Any', 35, 5),
+                               ('Hachette', 'Any', 33, 5),
+                               ('MediaStar', 'Any', 35, 5),
+                               ('NewIndiaBookSource', 'Any', 33, 5),
+                               ('OrientBlackSwan', 'Any', 33, 5),
+                               ('Oxford', 'Any', 30, 5),
+                               ('PearsonEducation', 'Any', 30, 5),
                                ('PopularPrakasham', 'Any', 20, 5),
-                               ('Prakash', 'Any', 20, 5),
-                               ('Prism', 'Any', 20, 5),
-                               ('RandomHouse', 'Any', 20, 5),
-                               ('ResearchPress', 'Any', 20, 5),
-                               ('Rupa', 'Any', 20, 5),
-                               ('Sage', 'Any', 20, 5),
-                               ('Unknown', 'Any', 20, 5);
+                               ('Prakash', 'Any', 33, 5),
+                               ('Prism', 'Any', 33, 5),
+                               ('RandomHouse', 'Any', 30, 5),
+                               ('ResearchPress', 'Any', 30, 5),
+                               ('Rupa', 'Any', 30, 5),
+                               ('Sage', 'Any', 30, 5),
+                               ('Vinayaka', 'Any', 30, 5),
+                               ('Westland', 'Any', 30, 5),
+                               ('CambridgeUniversityPress', 'Any', 30, 5),
+                               ('IndiaBooks', 'Any', 33, 5),
+                               ('Unknown', 'Any', 25, 5);
