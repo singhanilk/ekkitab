aec914c788dd0cced9bd7c4031b6b28624e75bfd
Vijay Raghavan <vijay@zen.(none)>
2010-07-03 17:11:04 +0530

    added capability to accept subject on command line.

--- bin/sendmail.php
+++ bin/sendmail.php
@@ -1,35 +1,48 @@
 <?php
-function sendmail($to,$body){
-//Default receiver's address can be specified here, for the moment christopher@ekkitab.com assumed
-    $receiver = $to;
-    if($receiver == ""){
-        $receiver = "christopher@ekkitab.com";
+
+define (DEFAULT_ADDRESSEE, "christopher@ekkitab.com");
+define (SENDER, "support@ekkitab.com");
+define (DEFAULT_SUBJECT, "Email from Ekkitab");
+
+    function sendmail($to, $subject, $body){
+
+        $sender = SENDER;
+        $headers = "From:$sender" . "\r\n" .  'X-Mailer: PHP/' . phpversion();
+
+        if (mail($to, $subject, $body, $headers)) {
+            return 0;
+        }
+        return 1;
     }
-    $subject = "Logs from updateprices";
-    $sender = "support@ekkitab.com";
-    $headers = "From:$sender" . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    echo "Sending update prices logs to $receiver\n";
-    if (mail($receiver, $subject, $body, $headers)) {
-        echo("Message successfully sent!\n");
-    } else {
-        echo("Message delivery failed...\n");
-    }
-}
-    $fh = fopen ("php://stdin","r") or die("Could not read from standard input");
-    $body = "";
-    while ($contents = fgets($fh)){
+
+    $subject = DEFAULT_SUBJECT;
+    $to = DEFAULT_ADDRESSEE;
+
+    for ($i=1; $i<$argc; $i++) {
+
+       switch($argv[$i]) {
+            case "-s":
+                   if ($i+1 >= $argc) {
+                      echo "Fatal: Subject specified but no value supplied.\n";
+                      echo "Usage: " . $argv[0] . " [ -s <subject> ] [ email address [, email address]... ]\n" ;
+                      exit(1);
+                   }
+                   else {
+                      $subject = $argv[$i+1];
+                      $i++;
+                   }
+                   break;
+
+            default:   
+                   $to = $argv[$i];
+                   break;
+       }
+   }
+
+   $fh = fopen ("php://stdin","r") or die ("Failed: Could not read from standard input.\n");
+   $body = "";
+   while ($contents = fgets($fh)){
         $body .= $contents;
-    }
-    if($body == ""){
-        echo "No Message Body!!\n ";
-    }
-    if (!isset($argv[1]) ){
-        echo "Usage $argv[0] <reciever1@domain.com,reciever2@domain.com,....>\nAs Reciever is not specified!! Assuming default\n";
-        $to = "";
-    }
-    else{
-        $to =  $argv[1];
-    }
-sendmail($to,$body)
+   }
+   sendmail($to, $subject, $body)
 ?>
