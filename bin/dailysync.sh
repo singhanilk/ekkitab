ef23032329c149f769229736d6dd045525fc9fe5
Vijay Raghavan <vijay@zen.(none)>
2010-07-05 11:09:06 +0530

    Progress reporting reduced to every 1 million books, instead of evry 10k books.

--- bin/dailysync.sh
+++ bin/dailysync.sh
@@ -5,17 +5,21 @@ if [ -z $EKKITAB_HOME ] ; then
 fi;
 echo "Starting daily sync routine."
 echo "Updating Ingram content..."
-( cd $EKKITAB_HOME/bin ; ./updateingram.sh | php sendmail.php -s "Status: Update Ingram" vijay@ekkitab.com )
+rundate=$(date +"%D-%T")
+( cd $EKKITAB_HOME/bin ; ./updateingram.sh | php sendmail.php -s "Daily Sync Report: [$rundate] Update Ingram" vijay@ekkitab.com )
 echo "Rsync'ing images with production server..."
 rsync -av --exclude=cache/* /mnt3/magento-product-images/ prod:/mnt3/magento-product-images > /mnt2/scm/logs/rsync.log
 echo "Updating prices..."
-( cd $EKKITAB_HOME/bin ; ./updateprices.sh | php sendmail.php -s "Status: Update Prices" vijay@ekkitab.com )
+rundate=$(date +"%D-%T")
+( cd $EKKITAB_HOME/bin ; ./updateprices.sh | php sendmail.php -s "Daily Sync Report: [$rundate] Update Prices" vijay@ekkitab.com )
 echo "Resetting reference database..."
 ( cd $EKKITAB_HOME/db ; ./reset_refdb.sh ) 
 echo "Initializing catalog..."
-( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -c ) 
+rundate=$(date +"%D-%T")
+( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -c | php ../sendmail.php -s "Daily Sync Report: [$rundate] Init Catalog - books" vijay@ekkitab.com ) 
 echo "Initializing prices..."
-( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -p ) 
+rundate=$(date +"%D-%T")
+( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -p | php ../sendmail.php -s "Daily Sync Report: [$rundate] Init Catalog - prices" vijay@ekkitab.com ) 
 echo "Indexing and updating production database..."
 ( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -u ) 
 echo "Completed daily sync routine."
