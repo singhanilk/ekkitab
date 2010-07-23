4e7c30c9167f8a625845c674fc033a10a592cfdf
Vijay Raghavan <vijay@zen.(none)>
2010-07-23 11:57:13 +0530

    Changed to accept email addresses from command line.

--- bin/stockstatusimport.sh
+++ bin/stockstatusimport.sh
@@ -3,9 +3,18 @@ if [ -z $EKKITAB_HOME ] ; then
     echo "EKKITAB_HOME is not set..."
     exit 1;
 fi;
+tomail=""
+while [ $# -gt 0 ] ; do 
+    tomail="$tomail $1"
+    shift
+done 
+if [ "$tomail" == "" ] ; then
+    tomail=vijay@ekkitab.com # Default recipient
+fi
+tomail=`echo $tomail`
 rundate=$(date +"%D-%T")
 z=`$EKKITAB_HOME/utils/autostockprocess.pl`
 if [ ! "$z" == "" ] ; then
-    ( cd $EKKITAB_HOME/bin ; echo "$z" | php sendmail.php -s "Stock Status Import Report: [$rundate]" vijay@ekkitab.com saran@ekkitab.com )
+    ( cd $EKKITAB_HOME/bin ; echo "$z" | php sendmail.php -s "Stock Status Import Report: [$rundate]" $tomail )
 fi
 
