00d9437ea107d7b4feb10845ba2ad82e27192747
Vijay Raghavan <vijay@zen.(none)>
2010-07-23 11:48:22 +0530

    Changed to get email addresses from command-line.

--- bin/dailysync.sh
+++ bin/dailysync.sh
@@ -3,23 +3,33 @@ if [ -z $EKKITAB_HOME ] ; then
     echo "EKKITAB_HOME is not set..."
     exit 1;
 fi;
+tomail=""
+while [ $# -gt 0 ] ; do 
+    tomail="$tomail $1"
+    shift
+done 
+if [ "$tomail" == "" ] ; then
+    tomail=vijay@ekkitab.com # Default recipient
+fi
+tomail=`echo $tomail`
+
 echo "Starting daily sync routine."
 echo "Updating Ingram content..."
 rundate=$(date +"%D-%T")
-( cd $EKKITAB_HOME/bin ; ./updateingram.sh | php sendmail.php -s "Daily Sync Report: [$rundate] Update Ingram" vijay@ekkitab.com )
+( cd $EKKITAB_HOME/bin ; ./updateingram.sh | php sendmail.php -s "Daily Sync Report: [$rundate] Update Ingram" $tomail )
 echo "Rsync'ing images with production server..."
 rsync -av --exclude=cache/* /mnt3/magento-product-images/ prod:/mnt3/magento-product-images > /mnt2/scm/logs/rsync.log
 echo "Updating prices..."
 rundate=$(date +"%D-%T")
-( cd $EKKITAB_HOME/bin ; ./updateprices.sh | php sendmail.php -s "Daily Sync Report: [$rundate] Update Prices" vijay@ekkitab.com )
+( cd $EKKITAB_HOME/bin ; ./updateprices.sh | php sendmail.php -s "Daily Sync Report: [$rundate] Update Prices" $tomail )
 echo "Resetting reference database..."
 ( cd $EKKITAB_HOME/db ; ./reset_refdb.sh ) 
 echo "Initializing catalog..."
 rundate=$(date +"%D-%T")
-( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -c | php ../bin/sendmail.php -s "Daily Sync Report: [$rundate] Init Catalog - books" vijay@ekkitab.com ) 
+( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -c | php ../bin/sendmail.php -s "Daily Sync Report: [$rundate] Init Catalog - books" $tomail ) 
 echo "Initializing prices..."
 rundate=$(date +"%D-%T")
-( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -p | php ../bin/sendmail.php -s "Daily Sync Report: [$rundate] Init Catalog - prices" vijay@ekkitab.com ) 
+( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -p | php ../bin/sendmail.php -s "Daily Sync Report: [$rundate] Init Catalog - prices" $tomail ) 
 echo "Indexing and updating production database..."
 ( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -u ) 
 # Restart Tomcat
@@ -48,5 +58,5 @@ echo "Transferring new catalog to production server..."
 #./synchcatalog.sh
 #exit
 #!
-echo "Completed daily sync routine."
+echo "Completed daily sync routine. New catalog transferred to production." 
 
