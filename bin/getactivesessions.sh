74b4d6a4abb9910b4e0d55d093d5abbf723dbc5c
Vijay Raghavan <vijay@zen.(none)>
2010-07-26 19:01:41 +0530

    echos number of sessions rather than set it as exit value.

--- bin/getactivesessions.sh
+++ bin/getactivesessions.sh
@@ -6,4 +6,4 @@ fi;
 . $EKKITAB_HOME/bin/db.sh 
 activesessions=`mysql -u $user -p$password -h $host ekkitab_books -e  "select count(distinct(session_id))  from log_visitor where last_visit_at >=(now() -INTERVAL 10 MINUTE)"` ; 
 activesessions=`echo ${activesessions##count(distinct(session_id))}`
-exit $activesessions
+echo $activesessions
