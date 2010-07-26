006a67680511516dd0c13b2e212a09f94c6aa3a2
Vijay Raghavan <vijay@zen.(none)>
2010-07-26 18:54:08 +0530

    script to collect active sessions on the website.

---
+++ bin/getactivesessions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z $EKKITAB_HOME ] ; then
+    echo "EKKITAB_HOME is not set..."
+    exit 1;
+fi;
+. $EKKITAB_HOME/bin/db.sh 
+activesessions=`mysql -u $user -p$password -h $host ekkitab_books -e  "select count(distinct(session_id))  from log_visitor where last_visit_at >=(now() -INTERVAL 5 MINUTE)"` ; 
+activesessions=`echo ${activesessions##count(distinct(session_id))}`
+exit $activesessions
