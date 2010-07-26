321594c13686507aa4a02bf1c361bc30eeac644b
Vijay Raghavan <vijay@zen.(none)>
2010-07-26 18:57:54 +0530

    increased session active period to 10 minutes.

--- bin/getactivesessions.sh
+++ bin/getactivesessions.sh
@@ -4,6 +4,6 @@ if [ -z $EKKITAB_HOME ] ; then
     exit 1;
 fi;
 . $EKKITAB_HOME/bin/db.sh 
-activesessions=`mysql -u $user -p$password -h $host ekkitab_books -e  "select count(distinct(session_id))  from log_visitor where last_visit_at >=(now() -INTERVAL 5 MINUTE)"` ; 
+activesessions=`mysql -u $user -p$password -h $host ekkitab_books -e  "select count(distinct(session_id))  from log_visitor where last_visit_at >=(now() -INTERVAL 10 MINUTE)"` ; 
 activesessions=`echo ${activesessions##count(distinct(session_id))}`
 exit $activesessions
