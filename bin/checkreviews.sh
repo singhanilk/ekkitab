4b55e0dbde58755bf0557dfe8053e6fc046e8e04
Vijay Raghavan <vijay@zen.(none)>
2010-07-27 20:06:28 +0530

    Completed the script to select and list reviews.

--- bin/checkreviews.sh
+++ bin/checkreviews.sh
@@ -4,20 +4,17 @@ if [ -z $EKKITAB_HOME ] ; then
     exit 1;
 fi;
 . $EKKITAB_HOME/bin/db.sh 
-tomail=""
-while [ $# -gt 0 ] ; do 
-    tomail="$tomail $1"
-    shift
-done 
-if [ "$tomail" == "" ] ; then
-    tomail=anisha@ekkitab.com # Default recipient
-fi
-
-tomail=`echo $tomail`
 rundate=$(date +"%D-%T")
 pendingreviewcount=`mysql -u $user -p$password -h $host ekkitab_books -e  "select count(*) from review where status_id=2"` ; 
 pendingreviewcount=`echo ${pendingreviewcount##count(*)}`
-z="Hi \n Below are the reviews which need your attention. Please login to admin to view and approve them.\n"
-reviews=`mysql -u $user -p$password -h $host ekkitab_books -e  "select rd.title,rd.nickname from review r, review_detail rd where r.review_id=rd.review_id and r.status_id=2"` ; 
-z= "$z \n Regards"
-echo "$z"
+if (( $pendingreviewcount > 0 )) ; then
+  echo -e "Dear Administrator,\nThe following reviews require clearance for display on the website, as of $rundate.\nPlease login to admin to view and approve them.\n-----------------------------------------------------------------------------"
+  for (( i=0; i<$pendingreviewcount; i++ )) ; do
+     review=`mysql -u $user -p$password -h $host ekkitab_books -e  "select rd.title,'->',rd.nickname from review r, review_detail rd where r.review_id=rd.review_id and r.status_id=2 limit $i,1"` ; 
+     review=`echo ${review##*nickname}`;
+     title=`echo ${review%%->*}`
+     name=`echo ${review##*->}`
+     echo "Review: '$title'  BY '$name'"
+  done
+  echo "-----------------------------------------------------------------------------"
+fi
