e2c5f4b36177dfafede5fb76721e9e683d682921
Nimil Christopher <chris@Arun-laptop.(none)>
2010-08-06 13:35:13 +0530

    Revert "bug[150]"
    This reverts commit 57a94b6cc0b7f636d541eabaca9acdf26a9aa84f.

--- bin/processors/ubs.pl
+++ bin/processors/ubs.pl
@@ -1,13 +1,4 @@
 #!/usr/bin/perl
-use Config::Abstract::Ini;
-my $ekkitab_home = $ENV{EKKITAB_HOME};
-if (!($ekkitab_home)){
-print "Not Defined" . "\n";
-}
-my $Settingsfile = $ekkitab_home . "/config/stockprocess.ini";
-my $settings     = new Config::Abstract::Ini($Settingsfile);
-my %values       = $settings -> get_entry('availability');
-my $threshold    = $values{'threshold'};
 print "#ISBN\tPRICE\tCURRENCY\tAVAILABILITY\tIMPRINT\tTITLE\tAUTHOR\n";
 $/=undef;
 open(FILE, $ARGV[0]) or die "cannot open input file";
@@ -41,7 +32,7 @@ for (my $i=0; $i<=$#isbns; $i++) {
         }
         my $availability = '';
         my $stock = $fields[$end];
-        if ($stock <= $threshold){
+        if ($stock <= 3){
            $availability = 'Not Available' . '[' . $stock . ']';
         }
         else{
