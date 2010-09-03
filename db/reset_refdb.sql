a777cdfff1ddc6a98868cf1e5ea782026db04a96
Administrator <admin@ppotula.local>
2010-09-03 14:51:49 +0530

    Issue Id:261:Stock processing should be enhanced to ignore a set of books sent by supplier/distributor

--- db/reset_refdb.sql
+++ db/reset_refdb.sql
@@ -1,4 +1,17 @@
-drop database if exists reference;
+-- @version 1.0     Sep 03, 2010
+-- Changes: The drop database was removed as we should not drop some of the 
+--          reference tables i.e. missing_isbns and ignore_isbns tables.
+
+-- drop database if exists reference;
+-- List all the tables that need to be dropped
+USE `reference`;
+drop table if exists books ;
+drop table if exists ek_bisac_category_map ;
+drop table if exists ek_currency_conversion ;
+drop table if exists ek_discount_setting ;
+drop table if exists ek_supplier_params ;
+drop table if exists book_availability ;
+drop table if exists books_promo ;
 source create_reference_db.sql;
 source ../data/refdb_categories.sql;
 source init_refdb.sql;
