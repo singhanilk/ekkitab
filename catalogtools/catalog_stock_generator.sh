d45c0edd2fcbe0c10c83636f5ba3100f5e72ee6b
Administrator <admin@ppotula.local>
2010-09-15 16:13:07 +0530

    Shell script which runs both catalog, stock and bisac

---
+++ catalogtools/catalog_stock_generator.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+echo "Running catalog and stock";
+excelFile=$1/$2.xls
+noBisacFile=$1/$2-nobisac.txt
+bisacFile=$1/$2.txt
+stockListFile=$1/newarrivals-$2-stocklist.txt
+preorder=$3
+perl processors/catalog_stock_generator.pl $excelFile $noBisacFile $stockListFile $preorder
+echo "Running biscac code generator";
+php bookcrawlers/bisaccodes.php $noBisacFile $bisacFile
+rm $noBisacFile
