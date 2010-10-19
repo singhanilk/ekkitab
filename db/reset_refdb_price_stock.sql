1eaa2f8f3123c740b2ff72c3c7a894c21b3d8e38
Administrator <admin@ppotula.local>
2010-10-19 14:17:11 +0530

    Fixed issues in the script

--- db/reset_refdb_price_stock.sql
+++ db/reset_refdb_price_stock.sql
@@ -2,4 +2,4 @@ use reference;
 --
 -- Reset the list_price and stock for all the books in the database before the prices are run.
 --
-update books set list_price = null and in_stock = 0;
+update books set list_price = null, in_stock = 0;
