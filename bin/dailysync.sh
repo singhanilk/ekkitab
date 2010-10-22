cd3be579c95cf2ef5efbf1c0f47376705a7dee1b
Vijay Raghavan <vijay@zen.(none)>
2010-10-22 12:48:43 +0530

    Added email from production server after release

--- bin/dailysync.sh
+++ bin/dailysync.sh
@@ -28,29 +28,29 @@ pecho "[$rundate] Resetting reference database..."
 ( cd $EKKITAB_HOME/db ; ./reset_refdb.sh ) 
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Updating catalog..."
-( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -c >>$outfilename 2>>$outfilename) 
+( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -c >>$outfilename 2>&1 ) 
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Running updating prices..."
-( cd $EKKITAB_HOME/bin ; ./updateprices.sh >>$outfilename 2>>$outfilename )
+( cd $EKKITAB_HOME/bin ; ./updateprices.sh >>$outfilename 2>&1 )
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Resetting price and stock..."
-( cd $EKKITAB_HOME/db ; ./reset_refdb_price_stock.sh >>$outfilename 2>>$outfilename )
+( cd $EKKITAB_HOME/db ; ./reset_refdb_price_stock.sh >>$outfilename 2>&1 )
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Updating catalog prices..."
-( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -p >>$outfilename 2>>$outfilename ) 
+( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -p >>$outfilename 2>&1 ) 
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Indexing and updating production database..."
-( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -u >>$outfilename 2>>$outfilename ) 
+( cd $EKKITAB_HOME/db ; ./initcatalog.sh ../config/catalog.cfg -u >>$outfilename 2>&1 ) 
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Updating global section product ids..."
-( cd $EKKITAB_HOME/db ; php ./load_globalsection_books.php ../data/globalsection.ini >>$outfilename 2>>$outfilename )
+( cd $EKKITAB_HOME/db ; php ./load_globalsection_books.php ../data/globalsection.ini >>$outfilename 2>&1 )
 # Restart Tomcat
-sudo service tomcat6 stop >>$outfilename 2>>$outfilename
+sudo service tomcat6 stop >>$outfilename 2>&1
 sleep 10
-sudo service tomcat6 start >>$outfilename 2>>$outfilename
+sudo service tomcat6 start >>$outfilename 2>&1
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Running smoke test on new catalog..."
-( cd $EKKITAB_HOME/bin; php bookValidation.php >>$outfilename 2>>$outfilename )
+( cd $EKKITAB_HOME/bin; php bookValidation.php >>$outfilename 2>&1 )
 success=$?
 if (( $success > 0 )) ; then
     pecho "Catalog validation failed. Will not continue."
@@ -59,7 +59,7 @@ fi
 # Release the new catalog
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Releasing new catalog to production.."
-( cd $EKKITAB_HOME/bin; ./release catalog >>$outfilename 2>>$outfilename )
+( cd $EKKITAB_HOME/bin; ./release catalog $tomail >>$outfilename 2>&1 )
 rundate=$(date +"%D-%T")
 pecho "[$rundate] Completed daily sync routine."
 rundate=$(date +"%D")
