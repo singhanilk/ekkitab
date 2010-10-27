cef920d9466a8a8b4d6c0b653dc4156f93753762
anisha <anisha@ekkitab.com>
2010-10-27 15:29:45 +0530

    data file for content article links

---
+++ magento/app/design/frontend/default/ekkitab/template/content/allfiles.php
@@ -0,0 +1,30 @@
+<?php 
+
+	function getArticles() {
+		Mage::log("here");
+		$articleXMLFile=Mage::getDesign()->getTemplateFilename("content/articlesindex.xml");
+		
+		if ( !file_exists($articleXMLFile) && !is_file($articleXMLFile) ) {
+			return null;
+		}
+		Mage::log("here after file exists...");
+        $xml = simplexml_load_file($articleXMLFile);
+        if (! $xml instanceof SimpleXmlElement) {
+            return(null);
+        }
+
+		$articles = array();
+        $filearticles = $xml->children();
+
+        foreach($filearticles as $filearticle) {
+            $article = array();
+            $article['title'] = (string)$filearticle['title'];
+            $article['image'] = (string)$filearticle['image'];
+            $article['url']   = (string)$filearticle['url'];
+            $article['description'] = trim($filearticle);
+            $articles[] = $article;
+        }
+
+		return $articles;
+	}
+?>
