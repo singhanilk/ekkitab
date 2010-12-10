32f58ad9187677fd632d578c31312a4ce5d467b7
Vijaya Raghavan <vijay@ekkitab.com>
2010-12-10 17:08:00 +0530

    corrected isbn number

--- magento/app/design/frontend/default/ekkitab/template/content/rohintonmistry.php
+++ magento/app/design/frontend/default/ekkitab/template/content/rohintonmistry.php
@@ -1,5 +1,5 @@
 <?php
 $selectParam = "isbn"; // data provided is isbn
 $pageTitle   = "Books by Rohinton Mistry"; // display title on page
-$isbns = array("9780571230570","9780571230556","9780571230587", "9780571230570");
+$isbns = array("9780571230570","9780571230556","9780571230587", "9780571230563");
 ?>
