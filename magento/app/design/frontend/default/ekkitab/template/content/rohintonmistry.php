c3f832e5abc42cd1589c9742444d2ff4541b5460
Vijaya Raghavan <vijay@ekkitab.com>
2010-12-10 16:50:32 +0530

    New front page item on Rohinton Mistry

--- magento/app/design/frontend/default/ekkitab/template/content/rohintonmistry.php
+++ magento/app/design/frontend/default/ekkitab/template/content/rohintonmistry.php
@@ -1,5 +1,5 @@
 <?php
 $selectParam = "isbn"; // data provided is isbn
 $pageTitle   = "Books by Rohinton Mistry"; // display title on page
-$isbns = array("9780679738718","9780375703423","9780571245888", "9780571230563", "9780679776321","9780571230587");
+$isbns = array("9780571230570","9780571230556","9780571230587", "9780571230570");
 ?>
